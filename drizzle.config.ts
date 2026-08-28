2d54c772709d8eb61cff3c19ebb80d1ca2cf14bc
S2 CORPORATIVO <adm@vetmg.com.br>
2026-08-28 22:40:54 +0000

    fix: include all Social OS schemas in Drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,7 +6,7 @@ if (!connectionString) {
 }
 
 export default defineConfig({
-  schema: "./drizzle/schema.ts",
+  schema: ["./drizzle/schema.ts", "./drizzle/socialOsSchema.ts", "./drizzle/socialGrowthSchema.ts"],
   out: "./drizzle",
   dialect: "mysql",
   dbCredentials: {
